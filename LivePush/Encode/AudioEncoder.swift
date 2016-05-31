46b86087141e8a805ae1b85dbdabba2ba3a0e020
swiftc-org <a@swiftc.org>
2016-05-31 13:23:57 +0800

    add flow description

---
+++ LivePush/Encode/AudioEncoder.swift
@@ -0,0 +1,21 @@
+//
+//  AudioEncoder.swift
+//  LivePush
+//
+//  Created by 成杰 on 16/5/31.
+//  Copyright © 2016年 swiftc.org. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+final class AudioEncoder: NSObject {
+    
+    private var audioQueue: AudioQueueRef!
+    
+    override init() {
+        super.init()
+        
+        audioQueue =
+    }
+}
