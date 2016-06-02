aa6b79810b7a0e8a27307cb0371909d911d09737
swiftc-org <a@swiftc.org>
2016-06-02 11:37:52 +0800

    start mix audio and video

---
+++ LivePush/Encode/AVMixer.swift
@@ -0,0 +1,17 @@
+//
+//  AVMixer.swift
+//  LivePush
+//
+//  Created by 成杰 on 16/6/2.
+//  Copyright © 2016年 swiftc.org. All rights reserved.
+//
+
+import UIKit
+
+class AVMixer: NSObject {
+    
+    override init() {
+        super.init()
+        
+    }
+}
